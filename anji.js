3667bab81902a1fad8867e7abaa201c758baf75b
AnnjiSri <143865039+AnnjiSri@users.noreply.github.com>
2023-09-08 20:19:04 +0530

    Add files via upload

---
+++ anji.js
@@ -0,0 +1,14 @@
+function anji(){
+var button1=document.getElementById("input1").value;
+      window.location.href = 'index.html';
+}
+function btn2(){
+    var button2=document.getElementById("likecount").value;
+            const likeCountElement=document.getElementById("likecount");
+            const anji1=document.getElementById("anji1");
+    let likes=0;
+    likes++;
+    likeCountElement.textContent = likes;
+}
+
+ 
---
+++ main.js
@@ -0,0 +1,8 @@
+
+function anji(){
+username=document.getElementById("input1").value;
+    localStorage.setItem("enter user name",username);
+    window.location="index1.html";
+}
+    
+   
